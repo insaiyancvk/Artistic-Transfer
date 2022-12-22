44eb93f08e6fc1de61af615c3b4f49f3c0594abb
insaiyancvk <cvamshik1@gmail.com>
2022-12-22 22:12:09 +0530

    basic webapp app layout

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_image_select import image_select
 from diffusers import StableDiffusionPipeline
+from time import sleep
 from dependencies import \
                   TransformerNetworkNN, \
                   VGG16, \
@@ -18,18 +19,24 @@ from dependencies import \
                   STYLE_WEIGHT, \
                   SAVE_MODEL_EVERY, \
                   SAVE_MODEL_PATH, \
-                  NUM_EPOCHS
+                  NUM_EPOCHS, \
+                  segments, \
+                  ttoi, \
+                  saveimg
+                  
 import torch.nn as nn
-import torch, os, glob, time
+import torch, os, glob, time, cv2, shutil
 from PIL import Image 
-
 
 num_cols = 3
 num_rows = 3
 
 st.title("Text Based Style Transfer On Segmented Images")
 
-with st.expander("Generate a Style Image"):
+if 'is_expanded_first' not in st.session_state:
+    st.session_state['is_expanded_first'] = True
+first_container = st.expander("Generate a Style Image", expanded=st.session_state['is_expanded_first'])
+with first_container:
   PROMPT = st.text_input("",placeholder="Enter a prompt to generate images")
   st.write('Example: "Japanese abstract art illustration"')
 
@@ -59,6 +66,12 @@ with st.expander("Generate a Style Image"):
 
       for x, i in enumerate(gen_imgs):
         i.save(f'Generated_images/{PROMPT}/{x}.jpg')
+      
+      if not os.path.exists(f"/content/drive/MyDrive/gen_imgs/{PROMPT}"):
+        os.mkdir(f"/content/drive/MyDrive/gen_imgs/{PROMPT}")
+      
+      for j,i in enumerate(gen_imgs):
+        i.save(f"/content/drive/MyDrive/gen_imgs/{PROMPT}/{j}.jpg")
 
     if len(gen_imgs) == 0:
       for f in glob.iglob(f"Generated_images/{PROMPT}/*"):
@@ -73,125 +86,207 @@ with st.expander("Generate a Style Image"):
     )
 
     if SELECTED_IMAGE:
-      st.write("Selected Image:")
+      st.header("Selected Image:")
       st.image(SELECTED_IMAGE)
-      SELECTED_IMAGE.save('style.jpg')
+    if st.button("Proceed further"):
+      with st.spinner('Saving the style image'):
+        SELECTED_IMAGE.save('style.jpg')
+  sleep(2)
+  st.session_state['is_expanded_first'] = False
 
-with st.expander("Train Transformer network"):
-  
-  TRAIN_TIME = st.slider('Select a training time', 30, 120, 5)
-  st.write("Set training time (min). Higher the training time, better the style transfer by the network.")
 
-  try:
-    del pipe
-  except:
-    pass
-  torch.cuda.empty_cache()
-  st.write('clearing cache data')
-
-  # Implement a button click for the following - "Initialize Network and dataset"
-  # Automate the below blocks of code
-  # -----------------------------------------------------------------------------------------------------------------------
-                                      # Stuff required to train a transformer network from scratch
+if os.path.exists("/content/train"):
   TransformerNetwork = TransformerNetworkNN().to(device)
-  if os.path.exists(f"/content/drive/MyDrive/gen_imgs/{PROMPT}/style.pth"):
-    TransformerNetwork.load_state_dict(torch.load(f"/content/drive/MyDrive/gen_imgs/{PROMPT}/style.pth", map_location=device))
-  VGG = VGG16('/content/vgg16.pth').to(device)
-  style_image = load_image(STYLE_IMAGE_PATH)
-  style_tensor = itot(style_image).to(device)
-  style_tensor = style_tensor.add(imagenet_neg_mean)
-  B, C, H, W = style_tensor.shape
-  style_features = VGG(style_tensor.expand([BATCH_SIZE, C, H, W]))
-  style_gram = {}
-  for key, value in style_features.items():
-      style_gram[key] = gram(value)
-
-  # Optimizer settings
-  optimizer = optim.Adam(TransformerNetwork.parameters(), lr=ADAM_LR)
-  # -----------------------------------------------------------------------------------------------------------------------
-                                      # If there's pretrained weights with the user, import that
-  # TRAIN_IMAGE_SIZE = 512
-  # DATASET_PATH = "/content/train"
-  # SEED = 68
-
-  # device = ("cuda" if torch.cuda.is_available() else "cpu")
-
-  # PROMPT = "Kazimir Malevich art in Retrowave style" #@param {type:"string"}
-  # # Load networks
-  # TransformerNetwork = TransformerNetworkNN().to(device)
-  # if os.path.exists(f"/content/drive/MyDrive/gen_imgs/{PROMPT}/style.pth"):
-  #   print("Loading the pretrained weights")
-  #   TransformerNetwork.load_state_dict(torch.load(f"/content/drive/MyDrive/gen_imgs/{PROMPT}/style.pth", map_location=device))
-  # -----------------------------------------------------------------------------------------------------------------------
-
-  # TODO: automate the above two steps
-
-  MODEL_NAME = "style"
-  DRIVE_PATH = f"/content/drive/MyDrive/gen_imgs/{PROMPT}"
-
-  # -----------------------------------------------------------------------------------------------------------------------
-                                                  # Training loop
-
-  batch_count = 1
-  TRAIN_TIME *= 60
-  start_time = time.time()
-  progress_bar = st.progress(0)
-
-  for content_batch, _ in train_loader:
-    curr_batch_size = content_batch.shape[0]
-    optimizer.zero_grad()
+  st.session_state['is_expanded_sec'] = False
+  if 'is_expanded_sec' not in st.session_state:
+    st.session_state['is_expanded_sec'] = True
+  sec_container = st.expander("Transformer network", expanded=st.session_state['is_expanded_sec'])
+  with sec_container:
+    if os.path.exists(f'Generated_images/{PROMPT}'):
+      try:
+        del pipe
+        torch.cuda.empty_cache()
+      except:
+        pass
     
-    content_batch = content_batch[:,[2,1,0]].to(device)
-    generated_batch = TransformerNetwork(content_batch)
-    content_features = VGG(content_batch.add(imagenet_neg_mean))
-    generated_features = VGG(generated_batch.add(imagenet_neg_mean))
+    option = st.selectbox(
+      'Select an option to proceed further',
+      ('Load a pretrained network', 'Train a transformer network'))
 
-    # Content Loss
-    MSELoss = nn.MSELoss().to(device)
-    content_loss = CONTENT_WEIGHT * MSELoss(content_features['relu2_2'], generated_features['relu2_2'])
+    if option == "Load a pretrained network":
+      pretrained_prompt = st.text_input("",placeholder="Enter the prompt was previously used for training the network")
+      if os.path.exists(f"/content/drive/MyDrive/gen_imgs/{pretrained_prompt}/style.pth"):
+        TRAIN_IMAGE_SIZE = 512
+        DATASET_PATH = "/content/train"
+        SEED = 68
+        device = ("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Style Loss
-    style_loss = 0
-    for key, value in generated_features.items():
-        s_loss = MSELoss(gram(value), style_gram[key][:curr_batch_size])
-        style_loss += s_loss
-    style_loss *= STYLE_WEIGHT
-
-    # Total Loss
-    total_loss = content_loss + style_loss
-
-    # Backprop and Weight Update
-    total_loss.backward()
-    optimizer.step()
-
-    if (((batch_count-1)%SAVE_MODEL_EVERY == 0) or (batch_count==NUM_EPOCHS*len(train_loader))):
-        torch.save(TransformerNetwork.state_dict(), f"{DRIVE_PATH}/{MODEL_NAME}.pth")
-    batch_count+=1
-    progress_bar.progress((time.time()-start_time)/TRAIN_TIME)
+        st.write("Loading the pretrained weights")
+        TransformerNetwork.load_state_dict(torch.load(f"/content/drive/MyDrive/gen_imgs/{pretrained_prompt}/style.pth", map_location=device))
     
-    if (time.time()-start_time) > TRAIN_TIME:
-      break
+    elif option == "Train a transformer network":
+      if os.path.exists("style.jpg"):
+        
+        TRAIN_TIME = st.slider('Select a training time', 30, 120, 5)
+        st.write("Set training time (min). Higher the training time, better the style transfer by the network.")
+        
+        if st.button("Start training"):
+          st.session_state['is_expanded_first'] = False
+          if os.path.exists(f"/content/drive/MyDrive/gen_imgs/{PROMPT}/style.pth"):
+            with st.spinner("Pretrained weights found. Loading it"):
+              TransformerNetwork.load_state_dict(torch.load(f"/content/drive/MyDrive/gen_imgs/{PROMPT}/style.pth", map_location=device))
+          VGG = VGG16('/content/vgg16.pth').to(device)
+          style_image = load_image(STYLE_IMAGE_PATH)
+          style_tensor = itot(style_image).to(device)
+          style_tensor = style_tensor.add(imagenet_neg_mean)
+          B, C, H, W = style_tensor.shape
+          style_features = VGG(style_tensor.expand([BATCH_SIZE, C, H, W]))
+          style_gram = {}
+          for key, value in style_features.items():
+              style_gram[key] = gram(value)
 
-  # -----------------------------------------------------------------------------------------------------------------------
+          # Optimizer settings
+          optimizer = optim.Adam(TransformerNetwork.parameters(), lr=ADAM_LR)
+          MODEL_NAME = "style"
+          DRIVE_PATH = f"/content/drive/MyDrive/gen_imgs/{PROMPT}"
+          batch_count = 1
+          TRAIN_TIME *= 60
+          start_time = time.time()
+          progress_bar = st.progress(0)
+          latest_iteration = st.empty()
 
-  """ TODO: 
-            - Initialize transformer network
-            - Move all classes and helper functions to a different file
-            - Training code
-  
-  """
+          for content_batch, _ in train_loader:
+            curr_batch_size = content_batch.shape[0]
+            optimizer.zero_grad()
+            
+            content_batch = content_batch[:,[2,1,0]].to(device)
+            generated_batch = TransformerNetwork(content_batch)
+            content_features = VGG(content_batch.add(imagenet_neg_mean))
+            generated_features = VGG(generated_batch.add(imagenet_neg_mean))
 
+            # Content Loss
+            MSELoss = nn.MSELoss().to(device)
+            content_loss = CONTENT_WEIGHT * MSELoss(content_features['relu2_2'], generated_features['relu2_2'])
+
+            # Style Loss
+            style_loss = 0
+            for key, value in generated_features.items():
+                s_loss = MSELoss(gram(value), style_gram[key][:curr_batch_size])
+                style_loss += s_loss
+            style_loss *= STYLE_WEIGHT
+
+            # Total Loss
+            total_loss = content_loss + style_loss
+
+            # Backprop and Weight Update
+            total_loss.backward()
+            optimizer.step()
+
+            if (((batch_count-1)%SAVE_MODEL_EVERY == 0) or (batch_count==NUM_EPOCHS*len(train_loader))):
+                torch.save(TransformerNetwork.state_dict(), f"{DRIVE_PATH}/{MODEL_NAME}.pth")
+            batch_count+=1
+            time_passed = time.time()-start_time
+            progress_time = time_passed/TRAIN_TIME
+            if progress_time < 1:
+              latest_iteration.text(f"{(time_passed/60):.1f}/{TRAIN_TIME//60} mins")
+              progress_bar.progress(progress_time)
+            else:
+              progress_bar.progress(1)
+
+            if (time.time()-start_time) > TRAIN_TIME:
+              latest_iteration.success('Training completed!', icon="✅")
+              break
+
+      else:
+        st.write("Generate and choose a style image")
+
+INDEX = None
+solutions = None
 
 with st.expander("Segment an Image"):
+
   image_file = st.file_uploader("Upload an image to segment", type=["png","jpg","jpeg"])
-
+  
   if image_file is not None:
+    with open("content.jpg","wb") as f: 
+      f.write(image_file.getbuffer())
 
-      file_details = {"filename":image_file.name, "filetype":image_file.type,
-                            "filesize":image_file.size}
-      st.write(file_details)
-      with open(os.path.join("content.jpg",image_file.name),"wb") as f: 
-        f.write(image_file.getbuffer())
+  elif image_file is None:
+    if os.path.exists('content.jpg'):
+      os.remove('content.jpg')
+    if os.path.exists('utils/segment.jpg'):
+      os.remove("utils/segment.jpg")
+      solutions = []
+    if os.path.exists('segments'):
+      shutil.rmtree('segments')
+
+  if os.path.exists("content.jpg") and (not os.path.exists("segments")):
+    if st.button("Generate segments"):
+      with st.spinner('Generating segments...'):
+        solutions = segments()
+        
+      st.write(type(solutions))
+
+    segimgs = []
+    for f in glob.iglob(f"segments/*"):
+      segimgs.append(Image.open(f))
+    
+  if len(solutions)>0:
+    INDEX = image_select(
+        label="Select a segment",
+        images=segimgs,
+        return_value="index",
+        captions=[str(i) for i in range(1,len(segimgs)+1)],
+        use_container_width = False
+    )
+    
+    if segimgs[INDEX]:
+      st.header("Selected Segment:")
+      segimgs[INDEX].save('utils/segment.jpg')
+      st.image("utils/segment.jpg")
 
 with st.expander("Style transfer on the segment"):
-  st.write("To be implemented")
+  device = "cuda" if torch.cuda.is_available() else "cpu"
+  st.write(solutions)
+  if os.path.exists("utils/segment.jpg"):
+    if st.button("Apply style transfer"):
+      with torch.no_grad():
+        torch.cuda.empty_cache()
+        content_image = load_image("utils/segment.jpg")
+        content_tensor = itot(content_image).to(device)
+        TransformerNetwork = TransformerNetwork.to(device)
+        generated_tensor = TransformerNetwork(content_tensor)
+        generated_image = ttoi(generated_tensor.detach())
+        saveimg(generated_image, "utils/segment_style.jpg")
+
+      with st.spinner("Applying style transfer"):
+        nimg = cv2.imread('content.jpg')[:, :, ::-1]
+        styleimg = cv2.imread('utils/segment_style.jpg')[:,:,::-1]
+        for i in solutions[INDEX]:
+          nimg[i[0]][i[1]] = styleimg[i[0]][i[1]]
+
+        img = Image.fromarray(nimg.astype('uint8'))
+        img.save('Final Transformation.png')
+  else:
+    st.write("Upload a content image to generate segments")
+
+if os.path.exists('Final Transformation.png'):
+
+  if 'final_trans' not in st.session_state:
+    st.session_state.final_trans = 'noimg'
+  st.image("Final Transformation.png")
+  st.session_state.final_trans = 'yeimg'
+
+  if st.session_state.final_trans == 'yeimg':
+    try:
+      if os.path.exists(f"/content/drive/MyDrive/gen_imgs/{PROMPT}"):
+        if st.button("Save content, style, transformed images to drive"):
+          all_imgs =  [
+            'Final Transformation.png',
+            'content.jpg',
+            'style.jpg'
+        ]
+        for i in all_imgs:
+          os.system(f"!cp {i} /content/drive/MyDrive/gen_imgs/{PROMPT}")
+    except NameError:
+      pass
--- streamlit/dependencies.py
+++ streamlit/dependencies.py
@@ -1,14 +1,11 @@
-import PIL
 from PIL import Image
-import matplotlib.pyplot as plt
-import os, cv2
-import torch
+import os, cv2, torch, json
 import torch.nn as nn
-import torch.optim as optim
 from torchvision import datasets, transforms, models
-import random
 import numpy as np
-import time
+from math import sqrt
+import torch.optim as optim
+import torch.nn.functional as F
 
 class VGG16(nn.Module):
     def __init__(self, vgg_path="vgg16.pth"):
@@ -225,3 +222,126 @@ train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE,
 imagenet_neg_mean = torch.tensor([-103.939, -116.779, -123.68], dtype=torch.float16).reshape(1,3,1,1).to(device)
 imagenet_mean = torch.tensor([103.939, 116.779, 123.68], dtype=torch.float16).reshape(1,3,1,1).to(device)
 
+## ----------------------------------------------     Segmentation stuff     ---------------------------------------------------------- ##
+from utils.yolact import Yolact
+from utils.output_utils import postprocess
+from utils.config import cfg
+import torch.backends.cudnn as cudnn
+
+def calc_size_preserve_ar(img_w, img_h, max_size):
+    ratio = sqrt(img_w / img_h)
+    w = max_size * ratio
+    h = max_size / ratio
+    return int(w), int(h)
+
+class FastBaseTransform(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        if torch.cuda.is_available():
+          self.mean = torch.Tensor((103.94, 116.78, 123.68)).float().cuda()[None, :, None, None]
+          self.std  = torch.Tensor( (57.38, 57.12, 58.40) ).float().cuda()[None, :, None, None]
+    
+        else:
+          self.mean = torch.Tensor((103.94, 116.78, 123.68)).float()[None, :, None, None]
+          self.std  = torch.Tensor( (57.38, 57.12, 58.40) ).float()[None, :, None, None]
+    
+        self.transform = cfg.backbone.transform
+    
+    def forward(self, img):
+        self.mean = self.mean.to(img.device)
+        self.std  = self.std.to(img.device)
+        
+        # img assumed to be a pytorch BGR image with channel order [n, h, w, c]
+        if cfg.preserve_aspect_ratio:
+            _, h, w, _ = img.size()
+            img_size = calc_size_preserve_ar(w, h, cfg.max_size)
+            img_size = (img_size[1], img_size[0]) # Pytorch needs h, w
+        else:
+            img_size = (cfg.max_size, cfg.max_size)
+
+        img = img.permute(0, 3, 1, 2).contiguous()
+        img = F.interpolate(img, img_size, mode='bilinear', align_corners=False)
+
+        if self.transform.normalize:
+            img = (img - self.mean) / self.std
+        elif self.transform.subtract_means:
+            img = (img - self.mean)
+        elif self.transform.to_float:
+            img = img / 255
+        
+        if self.transform.channel_order != 'RGB':
+            raise NotImplementedError
+        
+        img = img[:, (2, 1, 0), :, :].contiguous()
+
+        return img
+
+def evalimage(net:Yolact, path:str):
+    if torch.cuda.is_available():
+      frame = torch.from_numpy(cv2.imread(path)).cuda().float()
+    else:
+      frame = torch.from_numpy(cv2.imread(path)).float()
+    batch = FastBaseTransform()(frame.unsqueeze(0))
+    preds = net(batch)
+    h, w, _ = frame.shape
+    t = postprocess(preds, w, h, score_threshold = 0.15)
+    idx = t[1].argsort(0, descending=True)[:15]
+    masks = t[3][idx]
+    np.save('utils/mask_data', masks.cpu().numpy())
+
+def evaluate(net:Yolact, inp):
+    net.detect.use_fast_nms = True
+    net.detect.use_cross_class_nms = False
+    cfg.mask_proto_debug = False
+    evalimage(net, inp)
+    return
+
+
+def segments():
+    global solutions
+    with torch.no_grad():
+      if torch.cuda.is_available():
+          cudnn.fastest = True
+          torch.set_default_tensor_type('torch.cuda.FloatTensor')
+      else:
+          torch.set_default_tensor_type('torch.FloatTensor')
+
+      net = Yolact()
+      net.load_weights("yolact_base_54_800000.pth")
+      net.eval()
+
+      if torch.cuda.is_available():
+          net = net.cuda()
+
+      evaluate(net, "content.jpg")
+
+    mask = np.load('./utils/mask_data.npy')
+    sizes = {}
+    for x,i in enumerate(mask):
+        sizes[x] = len((np.argwhere(i!=0)))
+    sizes = {k: v for k, v in sorted(sizes.items(), key=lambda item: item[1], reverse=True)}
+
+    solutions = {}
+    for x,i in enumerate(list(sizes.keys())[:5]):
+        solutions[x] = np.argwhere(mask[i] != 0)
+
+    img = cv2.imread('content.jpg')[:, :, ::-1]
+
+    segments = {}
+    for i in range(len(solutions)):
+        segments[i] = np.full((img.shape), 255.)
+
+    for x,i in solutions.items():
+        for j in i:
+            segments[x][j[0]][j[1]] = img[j[0]][j[1]]
+
+    if not os.path.exists('segments'):
+        os.mkdir('segments')
+    
+    for i in range(len(segments)):
+        im = Image.fromarray(segments[i].astype('uint8'))
+        im.save(f'segments/{i}.jpg')
+    
+    return solutions
